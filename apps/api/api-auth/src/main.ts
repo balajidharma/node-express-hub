574e6d1007033f650be4d0580a1784be3f2c06de
balajidharma <balajidharma@hotmail.com>
2025-08-19 08:49:04 -0400

    Updated names

--- apps/api/api-auth/src/main.ts
+++ apps/api/api-auth/src/main.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import * as path from 'path';
 
-import authRoutes from './routes/authRoutes';
+import authRoutes from './routes/auth.routes';
 
 const app = express();
 
